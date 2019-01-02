883cdcc3f775ba11731e79237fed9917179aa83c
Elias Reis <eliasreis54@gmail.com>
2019-01-02 09:59:35 -0200

    Fixing the import request when body has a metadata attribute

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   flow_broker_url: process.env.FLOW_BROKER_URL || 'http://flowbroker:80', // export FLOW_BROKER_URL=http://172.19.0.3:80
-  device_manager_url: process.env.DEVICE_MANAGER_URL || 'http://device-manager:5000', // export DEVICE_MANAGER_URL=http://172.18.0.17:5000
+  device_manager_url: process.env.DEVICE_MANAGER_URL || 'http://device-manager:5000', // export DEVICE_MANAGER_URL=http://172.18.0.22:5000
   port: process.env.PORT || 3000,
 };
 

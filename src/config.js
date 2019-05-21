070909fa34567de74ce7b40b3f0ace5526fa9654
Raul Negreiros <rauln@cpqd.com.br>
2019-05-21 09:15:57 -0300

    Adds configuration to the request body limit
    default is 1mb

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const config = {
   device_manager_url: process.env.DEVICE_MANAGER_URL || 'http://device-manager:5000',
   cron_url: process.env.CRON_URL || 'http://cron:5000/cron',
   port: process.env.PORT || 3000,
+  request_body_limit: process.env.REQUEST_BODY_LIMIT || '1mb',
 };
 
 export default config;

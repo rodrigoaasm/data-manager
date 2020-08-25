377f5ca6755dbc04b451ffb51cbdad255fb63422
Mariane Previde <mprevide@cpqd.com.br>
2020-08-25 15:04:23 -0300

    Update publish.sh

--- travis/publish.sh
+++ travis/publish.sh
@@ -4,7 +4,10 @@ if [ $TRAVIS_PULL_REQUEST == false ] ; then
   if [ $TRAVIS_BRANCH != "master" ] ; then
     version=$TRAVIS_BRANCH
   fi
-  tag=$TRAVIS_REPO_SLUG:$version
+
+  DOCKER_TAG=$(echo ${version} | sed 's/\(.*\)\/\(.*\)/\1_\2/')
+
+  tag=${TRAVIS_REPO_SLUG}:${DOCKER_TAG}
 
   docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
   docker tag ${TRAVIS_REPO_SLUG} ${tag}

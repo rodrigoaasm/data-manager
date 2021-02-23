6e1b04f3e95f474cc761c959e675fb874c959f01
Mariane Previde <mprevide@cpqd.com.br>
2021-02-23 16:15:54 -0300

    Update deploy-gh-pages.sh

--- travis/deploy-gh-pages.sh
+++ travis/deploy-gh-pages.sh
@@ -2,22 +2,23 @@ git remote add -t gh-pages -f origin-gh-pages https://github.com/${TRAVIS_REPO_S
 git fetch origin-gh-pages
 git checkout gh-pages
 git checkout ${TRAVIS_BRANCH} -- ./docs
-rm -fr ./_static
-mv docs/* . -f
+mv docs/* . 
 git rm -r docs
 
 if [ "${TRAVIS_BRANCH}" == "master" ]
-then
+then 
   export VERSION="latest"
-else
+else 
   export VERSION="${TRAVIS_BRANCH}"
 fi
 
 docker run --volume $(pwd):/temp:Z dojot/aglio -i /temp/apiary.apib -o - > ./apiary_${VERSION}.html
 
+echo "Don't worry about the above errors"
+
 git add apiary_${VERSION}.html
-git commit -m 'Updating gh-pages' --amend
-git push --force http://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_REPO_SLUG} gh-pages
+git commit -m 'Updating gh-pages'
+git push http://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_REPO_SLUG} gh-pages
 git checkout -- .
 git clean -fd
 git checkout ${TRAVIS_BRANCH}
